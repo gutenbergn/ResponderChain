81c46fcbb3c3dfa07ac64f9a0b9e6e41c46043ea
Gutenberg Neto <gutenbergn@gmail.com>
2024-09-20 16:18:08 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,8 +7,8 @@ let package = Package(
     name: "ResponderChain",
     platforms: [
         .macOS(.v10_13),
-        .iOS(.v11),
-        .tvOS(.v11)
+        .iOS(.v13),
+        .tvOS(.v13)
     ],
     products: [
         .library(name: "ResponderChain", targets: ["ResponderChain"]),
