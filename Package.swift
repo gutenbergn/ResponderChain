3add5fd9a3b9ef0e7eb05c5290bdeb0e20a9c45a
Gutenberg Neto <gutenbergn@gmail.com>
2024-09-20 16:16:43 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "ResponderChain", targets: ["ResponderChain"]),
     ],
     dependencies: [
-        .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.2"),
+        .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", exact: "0.1.2"),
         .package(url: "https://github.com/Amzd/SwizzleSwift", from: "1.0.0"),
         .package(url: "https://github.com/nalexn/ViewInspector", from: "0.6.0"),
     ],
